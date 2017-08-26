fddbdc24fb8ba5ad300a9ece9ddb6efa825f9b1c
estrattonbailey <ericstrattonbailey@gmail.com>
2017-08-26 09:04:41 -0400

    Rewrite some fns to be a little cleaner, add comments

--- lib/util.php
+++ lib/util.php
@@ -2,28 +2,36 @@
 
 namespace h\util;
 
+/*
+ * Reduce array of attribute/value pairs
+ * into a single is_string
+ */
 function attrs($props){
-  $output = '';
-
-  foreach($props as $attr => $val){
-    $output .= $output."$attr='$val' ";
-  }
-
-  return $output;
+  return array_reduce(array_keys($props), function($res, $key) use ($props) {
+    $res .= "$key='$props[$key]'";
+    return $res;
+  }, '');
 }
 
-function isArray($arg){
-  return 'array' === gettype($arg);
-}
-
+/*
+ * Check if an $args value isProps
+ * an array of new props. If this Check
+ * fails, it either means we have a String
+ * child, or an array of components
+ * (hence the regex).
+ */
 function isProps( $arr ){
-  $firstVal = array_values($arr)[0];
+  $test = array_values($arr)[0];
 
-  return isArray($arr) 
-    && 'string' === gettype($firstVal)
-    && !preg_match("/<\/|\/>/", $firstVal);
+  return is_array($arr) 
+    && is_string($test)
+    && !preg_match("/<\/|\/>/", $test);
 }
 
+/*
+ * Merge two arrays, merging existing
+ * keys and values 
+ */
 function mergeProps($old, $new){
   $arr = [];
 
@@ -46,19 +54,35 @@ function mergeProps($old, $new){
 
 function createElement( $tag, $props = false ){
   return function( $args = '' ) use( $tag, $props ){
-    if (isArray($args) && isProps($args)){
+    /*
+     * If a user passes another set of props,
+     * merge them and and call createElement again
+     * (bc we're still waiting for a $children prop)
+     */
+    if (is_array($args) && isProps($args)){
       $newArgs = mergeProps($props, $args);
-
       return createElement($tag, $newArgs);
-    } else {
+    }
+    /*
+     * If it's not new props, OR the props failed
+     * the isProps() check, we're ready to return
+     * a component, or array of components.
+     */
+    else {
       $attrs = $props ? attrs($props) : '';
       $children = '';
 
-      if (isArray($args)){
+      if (is_array($args)){
+        /*
+         * Concat an array of child components
+         */
         foreach($args as $index => $child){
           $children .= $child;
         } 
       } else {
+        /*
+         * String as a child
+         */
         $children = $args;
       }
 
